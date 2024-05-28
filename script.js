2fec77f7130304abead392a280c29fa16c0239cb
nicotravaini <nicotravaini@gmail.com>
2024-05-28 18:43:42 +0200

    mejoras en sections 2 y 5

--- script.js
+++ script.js
@@ -40,3 +40,8 @@ menuColapse.forEach((element) => {
         list.classList.toggle("activeMenu");
     }
 })
+
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("form").reset();
+};
